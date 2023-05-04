913d1c1d1275eb2dc0844e0894642b0cf655fc7c
Hitesh Wadhwa <wadhwahitesh056@icloud.com>
2023-05-04 02:04:13 -0400

    Merging

--- src/FrontEnd/front_end.py
+++ src/FrontEnd/front_end.py
@@ -62,7 +62,6 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'json')
             self.end_headers()
             self.wfile.write(json.dumps({"ID":LEADER}).encode('utf-8'))
-<<<<<<< HEAD
         elif parsed_url[1] == "logVal":
             client_transID = parsed_url[-1].split(',')
             print(client_transID)
@@ -74,13 +73,11 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'text/plain')
             self.end_headers()
             self.wfile.write(response.encode('utf-8'))
-=======
         elif parsed_url[1] == "cache":
             self.send_response(200)
             self.send_header('Content-Type', 'json')
             self.end_headers()
             self.wfile.write(json.dumps({"cache":cache}).encode('utf-8'))
->>>>>>> 8027ed4fe0aa81a9ef7fa874e59a5b8cae64000d
             
 
 
